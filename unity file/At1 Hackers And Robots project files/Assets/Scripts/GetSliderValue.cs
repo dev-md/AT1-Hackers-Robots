1b25913aba994b3647e1731dabc8cbb39251589e
dev-md <30053790@tafe.wa.edu.au>
2022-08-02 12:24:34 +0800

    -Quick Script Update

--- unity file/At1 Hackers And Robots project files/Assets/Scripts/GetSliderValue.cs
+++ unity file/At1 Hackers And Robots project files/Assets/Scripts/GetSliderValue.cs
@@ -7,6 +7,8 @@ public class GetSliderValue : MonoBehaviour
 {
     private Slider slider;
     private Text text;
+    [SerializeField] private string mode = "Normal";
+
     private void Awake()
     {
         if(this.transform.parent.TryGetComponent(out Slider _slider))
@@ -20,6 +22,24 @@ public class GetSliderValue : MonoBehaviour
     }
     private void Update()
     {
-        text.text = slider.value.ToString();
+        switch (mode)
+        {
+            case "Normal":
+                text.text = slider.value.ToString("");
+                break;
+
+            case "F2":
+                text.text = slider.value.ToString("F2");
+                break;
+
+            case "%%":
+                text.text = (Mathf.Round(slider.value * 100f)).ToString("") + "%";
+                break;
+
+            default:
+                text.text = slider.value.ToString("");
+                break;
+
+        }
     }
 }
