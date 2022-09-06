6f005046c0616e8f52dbe34cc74ca1c0e18b1911
dev-md <30053790@tafe.wa.edu.au>
2022-09-06 12:14:52 +0800

    -Apply Feedback v2

--- unity file/At1 Hackers And Robots project files/Assets/Scripts/UI/EventSytemHandler.cs
+++ unity file/At1 Hackers And Robots project files/Assets/Scripts/UI/EventSytemHandler.cs
@@ -5,14 +5,23 @@ using UnityEngine.EventSystems;
 
 public class EventSytemHandler : MonoBehaviour
 {
-    EventSystem _eventSystem;
+    private EventSystem _eventSystem;
+    private BaseInputModule _inputModule;
     private void Awake()
     {
         _eventSystem = EventSystem.current;
+        _inputModule = GetComponent<BaseInputModule>();
     }
 
     public void ChangeSelectedButton(GameObject _gameObj)
     {
         _eventSystem.SetSelectedGameObject(_gameObj);
+        StartCoroutine(ScrTimer());
+    }
+    private IEnumerator ScrTimer()
+    {
+        _inputModule.enabled = false;
+        yield return new WaitForSeconds(0.25f);
+        _inputModule.enabled = true;
     }
 }
